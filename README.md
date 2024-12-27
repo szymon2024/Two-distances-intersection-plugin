4a885405f067095802b5665a88fd87034b71c59c
szymon2024 <142023542+szymon2024@users.noreply.github.com>
2024-12-28 00:48:02 +0100

    Add files via upload

---
+++ README.md
@@ -0,0 +1,8 @@
+
+TWO DISTANCES INTERSECTION PLUGIN FOR QGIS APPLICATION
+
+
+The plugin allows you get the intersection of two distances (2D Cartesian) and is created with QGIS 3.28 application. Tested with QGIS 3.28 and 3.34.
+
+Version 1.0.16
+![First look](firstLook.gif)
---
+++ __init__.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+# __init__.py  -  A Python Plugin Two distances intersection for QGIS
+#     begin             : 2023-07-18
+#     version           : 1.0.17
+#.....version date......: 2024-12-28
+#     author            : Szymon Kędziora
+
+def classFactory(iface):
+    from .twoDistancesIntersectionPlugin import TwoDistancesIntersectionPlugin
+    return TwoDistancesIntersectionPlugin(iface)
+
+
